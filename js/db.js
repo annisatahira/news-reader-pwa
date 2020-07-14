b980d00471f2ea83ba72d7f35aab7b563762eec5
annisatahira <annisatahira999@gmail.com>
2020-07-14 15:40:52 +0700

    add option save article

--- js/db.js
+++ js/db.js
@@ -10,3 +10,18 @@ var dbPromised = idb.open("news-reader", 1, function (upgradeDb) {
     unique: false,
   });
 });
+
+function saveForLater(article) {
+  dbPromised
+    .then(function (db) {
+      var tx = db.transaction("articles", "readwrite");
+      var store = tx.objectStore("articles");
+      console.log(article);
+      //method add for save article
+      store.add(article.result);
+      return tx.complete;
+    })
+    .then(function () {
+      console.log("Artikel berhasil di simpan.");
+    });
+}
